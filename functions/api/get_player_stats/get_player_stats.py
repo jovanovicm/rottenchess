4c3e5f9927f372c637b5ef81e3e16cfa3bc51a57
jovanovicm <jovanovicov@gmail.com>
2024-06-22 20:02:23 -0400

    API permissions

--- functions/api/get_player_stats/get_player_stats.py
+++ functions/api/get_player_stats/get_player_stats.py
@@ -1,6 +1,17 @@
 import json
 import boto3
 import os
+from decimal import Decimal
+
+def convert_decimals(item):
+    if isinstance(item, list):
+        return [convert_decimals(i) for i in item]
+    elif isinstance(item, dict):
+        return {k: convert_decimals(v) for k, v in item.items()}
+    elif isinstance(item, Decimal):
+        return int(item)
+    else:
+        return item
 
 def lambda_handler(event, context):
     PLAYER_STATS_TABLE = os.getenv('PLAYER_STATS_TABLE')
@@ -12,9 +23,10 @@ def lambda_handler(event, context):
     response = table.get_item(Key={'username': username})
     
     if 'Item' in response:
+        item = convert_decimals(response['Item'])
         return {
             'statusCode': 200,
-            'body': json.dumps(response['Item']),
+            'body': json.dumps(item),
             'headers': {
                 'Content-Type': 'application/json'
             }
