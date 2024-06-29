abae2bcf88b18986d47be21cad364766a693c79c
Bryan Fraschetti <bryan.fraschetti@gmail.com>
2024-06-29 19:15:01 -0400

    [Feature]: Updated loading animation

--- site/script.js
+++ site/script.js
@@ -81,12 +81,33 @@ async function fetchPlayersBatch(usernames) {
 
 function showLoading() {
   const leaderboardBody = document.getElementById("leaderboard-body");
-  leaderboardBody.innerHTML = '<tr><td colspan="7">Loading...</td></tr>';
+  leaderboardBody.innerHTML = `
+  <tr id="loading-container">
+    <td colspan="7">
+        <div class="loader loader--style3" title="2">
+            <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;"
+                xml:space="preserve">
+                <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+                    <animateTransform attributeType="xml"
+                        attributeName="transform"
+                        type="rotate"
+                        from="0 25 25"
+                        to="360 25 25"
+                        dur="0.75s"
+                        repeatCount="indefinite"
+                    />
+                </path>
+            </svg>
+        </div>
+    </td>
+  </tr>`;
 }
 
 function hideLoading() {
-  const loadingRow = document.querySelector("#leaderboard-body tr");
-  if (loadingRow && loadingRow.textContent.includes("Loading...")) {
+  const loadingRow = document.getElementById("loading-container");
+  if (loadingRow) {
     loadingRow.remove();
   }
 }
@@ -242,7 +263,7 @@ function filterByName() {
   applyFilters();
 }
 
-function sortTableByCurrentFilter() {
+async function sortTableByCurrentFilter() {
   const table = document.querySelector(".leaderboard");
   const headers = table.querySelectorAll("th");
 
